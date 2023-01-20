581f64d0d6c8edc5e8123006256b7db6b7794196
rpardue91 <107883991+rpardue91@users.noreply.github.com>
2023-01-20 13:07:27 -0500

    Add files via upload
    PS script to find PST file location and size on C: drive

---
+++ GetPst.ps1
@@ -0,0 +1,6 @@
+﻿$files = Get-ChildItem -Path C:\ -Filter "*.pst" -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Fullname
+
+Foreach($file in $files){
+    echo $file
+    (Get-Item -Path $file).Length/1MB
+}
